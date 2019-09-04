d2136e30717ba63f7ef627a2f88a0887dfe9b9db
ErikTheDeveloper <ImErikTheDeveloper@gmail.com>
2019-09-04 13:25:07 -0400

    added random numbers for each gem, created the click event and added the function to count the numbers.  having a NaN error and will debug but want to save first.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,15 +9,40 @@ $('#losses').text(loss);
 var currentScore = 0;
 var targetNumber;
 
-$("#currentScore").text(currentScore);
 
 targetNumber = Math.floor(Math.random() * 102) + 19;
 $('#targetNumber').text(targetNumber);
-
-
 
 // create variables for each stone being clicked (classified as ID's on the main page)
 var gem1;
 var gem2;
 var gem3;
 var gem4;
+
+gem1 = Math.floor(Math.random() * 12) + 1;
+$('#gem1').append("crystalValue", gem1);
+console.log(gem1);
+
+gem2 = Math.floor(Math.random() * 12) + 1;
+$('#gem2').append("crystalValue", gem2);
+console.log(gem2);
+
+gem3 = Math.floor(Math.random() * 12) + 1;
+$('#gem3').append("crystalValue", gem3);
+console.log(gem3);
+
+gem4 = Math.floor(Math.random() * 12) + 1;
+$('#gem4').append("crystalValue", gem4);
+console.log(gem4);
+
+// function for when an image gets clicked
+$(".GemPhoto4").on("click", function() {
+    var gemValue = ($(this).attr("crystalValue"));
+    // gemValue = parseInt(gemValue)
+
+    currentScore = currentScore + gemValue;
+
+    $('#currentScore').text(currentScore);
+    console.log(currentScore);
+
+});
